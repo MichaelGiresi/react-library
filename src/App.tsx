7433481e578f2ecb7a391f8934d2991dd0df426b
MichaelGiresi <michaelgiresidev@gmail.com>
2023-03-29 21:20:32 -0400

    HTML and bootstrap completed for homepage. fetch 2

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { HomePage } from './layouts/HomePage/HomePage';
+
+export const App = () =>  {
+  return (
+    <div>
+      <Navbar/>
+      <HomePage/>
+      <Footer/>
+    </div>
+  );
+}
+ 
---
+++ src/layouts/Utils/SpinnerLoading.tsx
@@ -0,0 +1,11 @@
+export const SpinnerLoading = () => {
+    return (
+        <div className="container d-flex justify-content-center align-items-center" style={{height: 550}}>
+            <div className="spinner-border text-primary" role='status'>
+                <span className="visually-hidden">
+                    Loading...
+                </span>
+            </div>
+        </div>
+    )
+}
